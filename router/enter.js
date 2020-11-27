03bbc596ca659e329129bc685fb711cb3562947d
DESKTOP-QOH3DOT\web <2995005931@qq.com>
2020-11-27 20:19:45 +0800

    新增娱乐设施接口

--- router/enter.js
+++ router/enter.js
@@ -4,5 +4,32 @@ const pool=require('../pool.js');
 // console.log(pool);
 //创建路由器对象
 const r=express.Router();
-
+//1.根据ktype查询酒吧和ktv  get  "/enter/v1/enterment/:ktype"  响应json 或者0
+r.get("/v1/enterment/:ktype",(req,res)=>{
+ var _ktype=req.params.ktype;
+ var sql="select * from enterment where ktype=?";
+ pool.query(sql,[_ktype],(err,result)=>{
+  if(err) throw err;
+  // console.log(result);
+  if(result.length>0){
+   res.send(result);
+ }else{
+  res.send("0");
+ }
+ });
+});
+//2.根据item查询酒吧附带套餐   get  "/enter/v1/item/"  响应json 或者0
+r.get("/v1/item",(req,res)=>{
+ var _item=req.params.item;
+ var sql="select * from item";
+ pool.query(sql,[_item],(err,result)=>{
+  if(err) throw err;
+  // console.log(result);
+  if(result.length>0){
+   res.send(result);
+ }else{
+  res.send("0");
+ }
+ });
+});
 module.exports=r;
